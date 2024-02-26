4b840d624fec6568f12c471cfa4cae9b08aaa166
Mariem <129475217+mariemkobbi@users.noreply.github.com>
2024-02-26 19:28:42 +0100

    Update publish.py

--- publish.py
+++ publish.py
@@ -5,34 +5,13 @@ import time
 # Define broker address and topic
 broker_address = "mqtt.eclipseprojects.io"
 topic = "TEST"
-counter = 1
-
-# # Sensor functions 
-# def read_phSensor():
-#     # Read sensor ph data and return a value
-#     return 9.5
-
-# def read_ecSensor():
-#     # Read sensor ec data and return a value
-#     return 20
-
-# # Build JSON data
-# data = {
-    
-#     "sensors": {
-#         "phValue": read_phSensor(),
-#         "ecValue": read_ecSensor()
-#     }
-# }
-
-# with open("data.json", "r") as f:
-#     data = json.load(f)
+counter = 0
 
 def generate_sensor_data():
     global counter
     with open("data.json", "r") as f:
         data = json.load(f)
-    counter += 1  # Increment counter before returning
+    counter += 1  
     return data
 # Create an MQTT client
 client = mqtt.Client()
@@ -41,13 +20,12 @@ client = mqtt.Client()
 client.connect(broker_address)
 
 while True:
-    # Publish the message every 30 minutes
+    # Publish the message every 5 minutes
     data = generate_sensor_data()
     client.publish(topic, json.dumps(data))
     print(f"Published data: {data}, counter: {counter}" )
-    print ("\ndata in-type ",type(data))
+    
+    time.sleep(300)  # Sleep 5 minutes
 
-    time.sleep(1)  # Sleep for 30 minutes (1 seconds)
-
-# Disconnect from the broker (optional, as the script runs indefinitely)
+# Disconnect from the broker (optional)
 client.disconnect()
